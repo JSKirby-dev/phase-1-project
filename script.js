b5e3b9cd3245ae548bd5a1c8c79586c8de422cfb
soloman <solomankirby@yahoo.com>
2022-10-10 16:02:27 +1100

    Begin HTML/CSS

--- script.js
+++
@@ -1,7 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
-const h3 = document.createElement("h3");
-h2.textContent = "This content added by JavaScript2";
-document.querySelector("body").appendChild(h3);
